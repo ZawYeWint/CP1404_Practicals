9999f612986e71954fe33180bcd43ad254af68a7
Zaw Ye Wint <zawyewint@gusto.edu.mm>
2022-04-07 10:09:53 +0800

    Answer the questions

---
+++ prac_02/randoms.py
@@ -0,0 +1,15 @@
+"""
+Line 1
+The smallest number I had seen was 6 and the largest was 17 in line 1.
+
+Line 2
+The smallest number I had seen was 5 and the largest was 9 in line 2.
+Line 2 didn't produce a 4.
+
+Line 3
+The smallest number I had seen was 2.7703567386991437 and the largest was 5.308192564047781 in line 3.
+
+"""
+
+import random
+print(random.randint(1, 100))

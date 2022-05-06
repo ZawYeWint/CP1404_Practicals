0325e944165ab11ad7bbc84aa32ad43354ec1c02
Zaw Ye Wint <zawyewint@gusto.edu.mm>
2022-05-06 18:58:57 +0800

    Using the information in the text entry field in the button handler function

--- prac_07/box_layout_demo.py
+++ prac_07/box_layout_demo.py
@@ -10,7 +10,12 @@ class BoxLayoutDemo(App):
 
     def handle_greet(self):
         print("test")
-        self.root.ids.output_label.text = "Hello"
+        self.root.ids.output_label.text = "Hello" + self.root.ids.input_name.text
+
+    # def handle_greet(self):
+    #    print("test")
+    #    your_name = self.root.ids.input_name.text
+    #    self.root.ids.output_label.text = "Hello" + your_name
 
 
 BoxLayoutDemo().run()

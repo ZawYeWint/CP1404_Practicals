bfe4596456fb0ccbf22f7685fd963b27baacab65
Zaw Ye Wint <zawyewint@gusto.edu.mm>
2022-05-12 10:13:57 +0800

    Print the details and the current fare

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -1,5 +1,6 @@
 from taxi import Taxi
 
+
 def main():
     # Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
     taxi = Taxi("Prius 1", 100, 1.23)
@@ -13,10 +14,15 @@ def main():
     print()
 
     # Restart the meter (start a new fare) and then drive the car 100 km
-    taxi.start_fare()   # reset the current distance fare to 0
+    taxi.start_fare()  # reset the current distance fare to 0
     print(f"Current fare: ${taxi.get_fare():.2f} before moving any new distance")
-    taxi.drive(100) # fuel left is 6 units (60km worth of distance) expected fare 1.23 * 60 = 73.80
+    distance_driven = taxi.drive(100)  # fuel left is 6 units (60km worth of distance) expected fare 1.23 * 60 = 73.80
+
     # Print the details and the current fare
+    print(f"After attempting to drive another 100km... Actual distance: {distance_driven} km")
+    print(taxi)
+    print(f"Current fare: ${taxi.get_fare():.2f}")
+
 
 if __name__ == '__main__':
     main()

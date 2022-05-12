d038f7eadbdaec17661cc274974ad73e557316af
Zaw Ye Wint <zawyewint@gusto.edu.mm>
2022-05-12 10:04:03 +0800

    Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km

---
+++ prac_08/taxi_test.py
@@ -0,0 +1,17 @@
+from taxi import Taxi
+
+def main():
+    # Create a new taxi with name "Prius 1", 100 units of fuel and price of $1.23/km
+    taxi = Taxi("Prius 1", 100, 1.23)
+
+    # Drive the taxi 40 km
+
+    # Print the taxi's details and the current fare
+
+
+    # Restart the meter (start a new fare) and then drive the car 100 km
+
+    # Print the details and the current fare
+
+if __name__ == '__main__':
+    main()

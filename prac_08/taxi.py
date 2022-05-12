16ff9c07d2057495e3909caa5f4bebff27cf3c36
Zaw Ye Wint <zawyewint@gusto.edu.mm>
2022-05-12 10:20:26 +0800

    Specialised version of a Car that includes fare costs

--- prac_08/taxi.py
+++ prac_08/taxi.py
@@ -7,11 +7,11 @@ from prac_08.car import Car
 
 class Taxi(Car):
     """Specialised version of a Car that includes fare costs."""
+    price_per_km = 1.23  # class variable, not a constant
 
-    def __init__(self, name, fuel, price_per_km):
+    def __init__(self, name, fuel):
         """Initialise a Taxi instance, based on parent class Car."""
         super().__init__(name, fuel)
-        self.price_per_km = price_per_km
         self.current_fare_distance = 0
 
     def __str__(self):
@@ -19,6 +19,7 @@ class Taxi(Car):
         return "{}, {}km on current fare, ${:.2f}/km".format(super().__str__(),
                                                              self.current_fare_distance,
                                                              self.price_per_km)
+        # self.price_per_km now refers to the class variable
 
     def get_fare(self):
         """Return the price for the taxi trip."""

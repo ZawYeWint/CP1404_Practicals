02cdf09c3780a1e80deeaffcada1dcb4600eb810
Zaw Ye Wint <zawyewint@gusto.edu.mm>
2022-05-12 10:04:54 +0800

    Print the taxi's details and the current fare

--- prac_08/taxi_test.py
+++ prac_08/taxi_test.py
@@ -8,7 +8,7 @@ def main():
     taxi.drive(40)
 
     # Print the taxi's details and the current fare
-
+    print(taxi)
 
     # Restart the meter (start a new fare) and then drive the car 100 km
 

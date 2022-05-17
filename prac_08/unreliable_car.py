93b461692ec01d9acd3382d350860d87bfb25154
Zaw Ye Wint <zawyewint@gusto.edu.mm>
2022-05-18 00:29:32 +0800

    Generating a random number between 0 and 100,and only driving the car if that number is less than the car's reliability

--- prac_08/unreliable_car.py
+++ prac_08/unreliable_car.py
@@ -7,3 +7,10 @@ class UnreliableCar(Car):
         """Initialising an UnreliableCar."""
         super().__init__(name, fuel)
         self.reliability = reliability
+
+    def drive(self, distance):
+        """Generating a random number between 0 and 100,
+        and only driving the car if that number is less than the car's reliability"""
+        random_number = randint(0, 100)
+        if random_number >= self.reliability:
+            distance = 0

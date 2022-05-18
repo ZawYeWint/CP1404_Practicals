0de6805d09dabcd03b565bfcadef8be2a664a1df
Zaw Ye Wint <zawyewint@gusto.edu.mm>
2022-05-19 02:25:30 +0800

    Returning a string representation of a SilverServiceTaxi

--- prac_08/silver_service_taxi.py
+++ prac_08/silver_service_taxi.py
@@ -9,3 +9,9 @@ class SilverServiceTaxi(Taxi):
         super().__init__(name, fuel)
         self.fanciness = fanciness
         self.price_per_km *= fanciness
+
+    def __str__(self):
+        """Returning a string representation of a SilverServiceTaxi."""
+        return f"{super().__str__()} plus flagfall of ${self.flagfall:.2f}"
+
+
